01f6120b9a574a19eab9617a85db1767e9b88e7a
Allan Lucas <allslages@gmail.com>
2026-03-12 10:57:41 +0000

    Concluído

--- exercises/ex_1/1.1.c
+++ exercises/ex_1/1.1.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-#define qtd_alunos 30
+#define qtd_alunos 5
 #define nota_min 0.0
 #define nota_max 10.0
 
 void inserir_notas(double v[], int n);
 void calculomedia(double v[], double a[], double b[], int n);
 double media_todos(double v[], int n);
-void maiorq_media(double v[], int n, double a);
+void maiorq_media(double v[],double k[], double p[], int n, double a);
 
 int main () {
     double nota1[qtd_alunos], nota2[qtd_alunos], media[qtd_alunos], media_turma;
+    printf("Insira a primeira nota dos estudantes:\n");
     inserir_notas(nota1, qtd_alunos);
+    printf("\n Insira a segunda nota dos estudantes:\n");
     inserir_notas(nota2, qtd_alunos);
     calculomedia(media, nota1, nota2, qtd_alunos);
     media_turma = media_todos(media, qtd_alunos);
-    printf("A média da turma é: %lf", media_turma);
-    maiorq_media(media, qtd_alunos, media_turma);
+    printf("\nA média da turma é: %.2f\n", media_turma);
+    maiorq_media(media, nota1, nota2, qtd_alunos, media_turma);
 }
 
 void inserir_notas(double v[], int n) {
@@ -41,10 +43,10 @@ double media_todos(double v[], int n) {
     return val_soma / (double) n;
 }
 
-void maiorq_media(double v[], int n, double a) {
+void maiorq_media(double v[],double k[], double p[], int n, double a) {
     for (int i = 0; i < n; i += 1) {
         if (v[i] > a){
-            printf("A nota do estudante número %d é maior que a média total da turma: %lf\n", i + 1, v[i]);
+            printf("A nota do estudante número %d é maior que a média total da turma: %.2f\n Nota 1: %.2f Nota 2: %.2f\n", i + 1, v[i], k[i], p[i]);
         }
     }
 }
