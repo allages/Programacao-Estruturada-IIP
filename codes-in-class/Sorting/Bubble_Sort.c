7a494294dae37a2b15751c7ba80fc9fb7a6438c6
Allan Lucas <allslages@gmail.com>
2026-03-16 11:10:45 +0000

    Bubble Sort (Incompleto)

---
+++ codes-in-class/Sorting/Bubble_Sort.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void bubbleSort(double v[], int n) {
+    int i, fim, pos, troca;
+    troca = 1; fim = n-2; pos = 0;
+    while (troca == 1) {
+        troca = 0;
+        for (i = 0; i <= fim; i += 1){
+            if (v[i] > v[i+1]){
+            }
+        }
+    }
