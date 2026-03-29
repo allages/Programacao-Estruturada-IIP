b94ab8b9dc680dc4790697a900d150a048481548
allagez <allslages@gmailcom>
2026-03-29 18:09:45 -0300

    menu em construção

--- codes-in-class/Manipular_Matriz.c
+++ codes-in-class/Manipular_Matriz.c
@@ -1,46 +1,46 @@
+// O programa tem como objetivo exercitar a manipulação de matrizes. Então foi feito um menu para utilizarmos as respectivas
+// funções.
+
 #include <stdio.h>
 #include <stdlib.h>
 #define LIN 5
 #define COL 5
 
-void preecherMatriz(m[][COL], int lin, int col);
-void imprimirMatriz(m[][COL], int lin, int col);
-void maiorValorMatriz(m[][COL], int lin, int col);
-void igualAZeroSecundaria(m[][COL], int lin. int col);
+void preencherMatriz(int m[][COL], int lin, int col);
+void imprimirMatriz(int m[][COL], int lin, int col);
+void menu(int m[][COL], int lin, int col);
 
 int main(){
     int matriz[LIN][COL];
     srand(1);
+    preencherMatriz(matriz, LIN, COL);
     imprimirMatriz(matriz, LIN, COL);
-    igualAZeroSecundaria(matriz, LIN, COL);
+    menu(matriz, LIN, COL);
 }
 
-void preencherMatriz(m[][COL], int lin, int col){
+void preencherMatriz(int m[][COL], int lin, int col){
     for (int i = 0; i < lin; i += 1){
         for (int j = 0; j < col; j += 1){
-            m[i][j] = rand() 31;
+            m[i][j] = rand() % 31;
         }
     }
 }
 
-void imprimirMatriz(m[][COL], int lin, int col){
+void imprimirMatriz(int m[][COL], int lin, int col){
     for (int i = 0; i < lin; i += 1){
-        for (int j = 0; j < lin; j += 1){
-            printf("%d\t" &m[i][j]);
+        printf("|");
+        for (int j = 0; j < col; j += 1){
+            printf("%2d ", m[i][j]);
         }
+        printf("|");
         printf("\n");
     }
 }
 
-void igualAZeroSecundaria(m[][COL], int lin, int col){
-    if (lin != col){
-        printf("Essa matriz não é quadrada!")
-        return;
-    }
-    for (int i = 1; i < lin; i += 1){
-        for (int j = 0; j > i; j += 1){
-            m[i][j] = 0;
-            }
-        }
-    }
+void menu(int m [][COL], int lin, int col){
+   int opcao = -1;
+    do {
+        printf("\n================MENU================\n[1]—Imprimir maior valor e posição.\n[2]—Imprimir menor valor e posiçao.\n[0]—Encerrar Aplicação.\n— ");
+        scanf("%d", &opcao);
+    } while (opcao != 0); 
 }
