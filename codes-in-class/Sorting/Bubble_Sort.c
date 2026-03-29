48fd8c58e3bfe3aa9cc1bb39b7e76b0aa1d4fb24
allagez <allslages@gmailcom>
2026-03-29 15:48:59 -0300

    Bubble Sort Completo

--- codes-in-class/Sorting/Bubble_Sort.c
+++ codes-in-class/Sorting/Bubble_Sort.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void bubbleSort(double v[], int n) {
-    int i, fim, pos, troca;
-    troca = 1; fim = n-2; pos = 0;
-    while (troca == 1) {
-        troca = 0;
-        for (i = 0; i <= fim; i += 1){
-            if (v[i] > v[i+1]){
+void preencherVetor(int v[], int n);
+void imprimirVetor(int v[], int n);
+void bubbleSort(int v[], int n);
+
+int main(){
+    int tamanhoVetor;
+    printf("Quantos valores serão inseridos no vetor(2-10)? ");
+    scanf("%d", &tamanhoVetor);
+    int vetor[tamanhoVetor];
+    preencherVetor(vetor, tamanhoVetor);
+    printf("Seu vetor não ordenado: ");
+    imprimirVetor(vetor, tamanhoVetor);
+    bubbleSort(vetor, tamanhoVetor);
+    printf("\nSeu vetor ordenado utilizando Bubble Sort: ");
+    imprimirVetor(vetor, tamanhoVetor);
+}
+
+void preencherVetor(int v[], int n){
+    srand(1);
+    int j = n;
+    for (int i = 0; i < n; i+= 1){
+        v[i] = rand() % 51;
+    }
+}
+
+void imprimirVetor(int v[], int n){
+    printf("[");
+    for(int i = 0; i < n; i += 1){
+        printf(" %d ", v[i]);
+    }
+    printf("]");
+}
+
+void bubbleSort(int v[], int n){
+    int tmp;
+    for (int i = 0; i < n - 1; i += 1){ // Serão feitas n - 1 varreduras (Exemplo: se o vetor for tamanho 10, 9 varreduras)
+        for (int j = 0; j < n - i - 1; j += 1){
+            if (v[j] > v[j + 1]){
+                tmp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = tmp;
             }
         }
     }
+}
