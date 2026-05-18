23e0350f28f38d0a22189082831cd85ba5f41824
allages <allslages@gmail.com>
2026-05-18 10:53:39 -0300

    gitignore + código completo + txt

--- codes-in-class/Struct e Pointers/ExerciseSctruct/2QuestaoStruct.c
+++ codes-in-class/Struct e Pointers/ExerciseSctruct/2QuestaoStruct.c
@@ -31,7 +31,7 @@ void preencherStruct(struct Produto *p, int i){
     printf("\n——————CADASTRO DE PRODUTO——————\n");
     printf("\nDigite o código do produto: ");
     scanf("%d", &p[i].codigo);
-    printf("\nDigite a descrição do produto");
+    printf("\nDigite a descrição do produto: ");
     lerStr(p[i].descricao, CARACTERES);
     lerStr(p[i].descricao, CARACTERES);
     printf("\nDigite o valor unitário do produto: ");
@@ -40,24 +40,35 @@ void preencherStruct(struct Produto *p, int i){
     scanf("%d", p[i].qtd_estoque);
 }
 
+void imprimirStruct(struct Produto *p, int tamanho){
+    printf("\n——————IMPRESSÃO DE PRODUTOS——————\n");
+    for (int i = 0; i < tamanho; i += 1){
+        printf("Produto %d", i + 1);
+        printf("\nCódigo: %d\n", p[i].codigo);
+        printf("Descrição: %s\n", p[i].descricao);
+    }
+}
 
 int main(){
     struct Produto produto[TAM];
     int entrada = 1;
     int i = 0;
     do {
-        printf("—————— MENU ——————\nSelecione:\n[1] — Cadastrar um novo produto\n[2] — Exibir dados dos produtos\n[0] — Encerrar programa\nInsira uma opção válida:");
+        printf("—————— MENU ——————\nSelecione:\n[1] — Cadastrar um novo produto\n[2] — Exibir código e descrição dos produtos\n[0] — Encerrar programa\nInsira uma opção válida: ");
         scanf("%d", &entrada);
         if (entrada == 1){
-            preencherStruct(produto, i);
-            i += 1;
+            if (i >= 40){
+                printf("\nLIMITE DE DADOS ATINGIDO\n");
+                continue;
+            }
+                preencherStruct(produto, i);
+                i += 1;
         } else if (entrada == 2){
-
+            imprimirStruct(produto, TAM);
+            printf("\n");
         } else if (entrada != 0){
             printf("\nERRO: Insira uma opção válida!\n");
         }
     } while (entrada != 0);
-
-
     return 0;
 }
